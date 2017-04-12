ed141b1cf264e33003146385d9bd96a47ae8b6ef
when Sunset <a1018998632@gmail.com>
2017-04-12 20:45:52 +0800

    上传

--- books/src/main/java/com/example/IntroductionToAlgorithms/util/Util.java
+++ books/src/main/java/com/example/IntroductionToAlgorithms/util/Util.java
@@ -35,6 +35,17 @@ public class Util {
         return bytes;
     }
 
+    public static byte[] getRandomByteListHasBounderSeedRandom(int size, int bounder){
+        Random random=new Random(System.currentTimeMillis());
+        byte[] bytes=new byte[size];
+        for (int i = 0; i < size; i++) {
+            if (bounder!=0)bytes[i]=(byte)random.nextInt(bounder);
+            else bytes[i]= (byte) ((byte) random.nextInt()+1);
+        }
+        Util.printList(bytes);
+        return bytes;
+    }
+
     public static void printList(int[] ints){
         for (int i:ints) System.out.print(i+",");
         System.out.println("");
